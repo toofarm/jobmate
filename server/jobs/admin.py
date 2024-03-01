734280ab9490e0ea1a15e16c6388e9ab7a543596
BleepBoop <Shaners@A-rusty-CB-radio-2.local>
2024-02-29 17:14:30 -0800

    update models and migrations

--- server/jobs/admin.py
+++ server/jobs/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import User, Job, Interaction, Company
+from .models import Job, Interaction, Company
 
-# Register your models here.
-admin.site.register(User)
 admin.site.register(Job)
 admin.site.register(Interaction)
 admin.site.register(Company)
--- server/jobs/models.py
+++ server/jobs/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100, blank=False, null=False)
-    email = models.EmailField(max_length=100, blank=False, null=False)
-    password = models.CharField(max_length=100, blank=False, null=False)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
+from django.contrib.auth.models import User
 
 
 class Job(models.Model):
@@ -20,7 +10,7 @@ class Job(models.Model):
         LOW = "Low", "Low"
 
     title = models.CharField(max_length=100, blank=False, null=False)
-    company = models.CharField(max_length=100, blank=False, null=False)
+    company = models.ForeignKey("Company", on_delete=models.CASCADE)
     description = models.TextField()
     link = models.URLField(max_length=200)
     contact = models.CharField(max_length=100, blank=True, null=True)
