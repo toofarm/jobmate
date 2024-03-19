6420d1f4febd74d6dc8255b4f787e738f43dcc3a
BleepBoop <Shaners@A-rusty-CB-radio-2.local>
2024-03-19 16:11:53 -0700

    add User forms to admin panel

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ["email", "username", "first_name", "last_name"]
+
+
+admin.site.register(User, CustomUserAdmin)
